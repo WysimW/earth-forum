481ce0cdc65aacbc52d22126bc16a5f5051bf771
WysimW <dupezthomas@gmail.com>
2025-04-17 20:30:07 +0200

    add role maker

--- backend/src/Form/UserType.php
+++ backend/src/Form/UserType.php
@@ -2,22 +2,31 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use App\Entity\Role;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -51,13 +60,30 @@ class UserType extends AbstractType
             ->add('playerRoles', EntityType::class, [
                 'class' => Role::class,
                 'choice_label' => 'name',
-                'label' => 'Rôles',
+                'label' => 'Rôles de jeu',
                 'required' => false,
                 'multiple' => true,
                 'expanded' => true,
                 'attr' => ['class' => 'role-checkbox-group'],
                 'help' => 'Sélectionnez un ou plusieurs rôles pour cet utilisateur'
             ]);
+            
+        // Ajout du champ pour les rôles système (Symfony)
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            $builder->add('roles', ChoiceType::class, [
+                'label' => 'Rôles système',
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Modérateur' => 'ROLE_MODERATOR',
+                    'Animateur' => 'ROLE_ANIMATOR',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => ['class' => 'system-role-checkbox-group'],
+                'help' => 'Définit les permissions de l\'utilisateur sur le forum'
+            ]);
+        }
             
         // Ajout du champ password uniquement lors de la création d'un nouvel utilisateur
         if ($options['is_creation']) {
