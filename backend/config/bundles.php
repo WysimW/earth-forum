b3c19a218f42fc0178a4597bf701c311758b31fe
WysimW <dupezthomas@gmail.com>
2025-04-19 01:57:24 +0200

    some change

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -4,8 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-// Correct : uniquement en dev et test
-Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
